113c2eeb875b8d20337a2babe200c3270ac5016e
LucaVilotta <lucavilotta@hotmail.com>
2023-04-30 17:46:21 -0300

    conección con el backend en el componente titulo-main

--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { persona } from "../model/persona.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,9 @@ export class PortfolioService {
 
   constructor(private http:HttpClient) { }
 
-  server = "http://localhost:3000/info";
+  server = "http://localhost:8080/personas/traer/perfil";
 
   obtenerDatos():Observable<any>{
-    return this.http.get(this.server)
+    return this.http.get<persona>(this.server)
   }
 }
--- src/app/titulo-main/titulo-main.component.ts
+++ src/app/titulo-main/titulo-main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PortfolioService } from '../servicios/portfolio.service';
+import { persona } from '../model/persona.model';
 
 @Component({
   selector: 'app-titulo-main',
@@ -7,13 +8,12 @@ import { PortfolioService } from '../servicios/portfolio.service';
   styleUrls: ['./titulo-main.component.css','./iconos.css']
 })
 export class TituloMainComponent {
-  
+  persona: persona = new persona("","","","","");
   constructor(private datosPortfolio:PortfolioService) {}
-  miPortfolio:any;
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data => {
       console.log(data)
-      this.miPortfolio=data;
+      this.persona=data;
     });
   }
 
